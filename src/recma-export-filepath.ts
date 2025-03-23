b99f22dda4031d1a86f36ed4e81d091fb0e8de54
Remco Haszing <remcohaszing@gmail.com>
2025-03-23 20:58:32 +0100

    Convert to TypeScript

--- src/recma-export-filepath.ts
+++ src/recma-export-filepath.ts
@@ -1,29 +1,35 @@
-/**
- * @import { Program } from 'estree'
- * @import { Plugin } from 'unified'
- */
-
 import { relative, resolve } from 'node:path'
 
+import { type Program } from 'estree'
 import { name as isIdentifierName } from 'estree-util-is-identifier-name'
 import normalizePath from 'normalize-path'
+import { type Plugin } from 'unified'
 
-/**
- * @typedef RecmaExportFilepathOptions
- * @property {boolean} [absolute=false]
- *   If true, use an absolute path. By default a relative path is used.
- * @property {string} [cwd]
- *   The current working directory to use when generating a relative file path.
- * @property {string} [name='filepath']
- *   The name to export the file path as.
- */
+interface RecmaExportFilepathOptions {
+  /**
+   * If true, use an absolute path. By default a relative path is used.
+   *
+   * @default false
+   */
+  absolute?: boolean
+
+  /**
+   * The current working directory to use when generating a relative file path.
+   */
+  cwd?: string
+
+  /**
+   * The name to export the file path as.
+   *
+   * @default 'filepath'
+   */
+  name?: string
+}
 
 /**
  * A recma plugin to expose the filepath as a named export
- *
- * @type {Plugin<[RecmaExportFilepathOptions?], Program>}
  */
-const recmaExportFilepath = (options = {}) => {
+const recmaExportFilepath: Plugin<[RecmaExportFilepathOptions?], Program> = (options = {}) => {
   const { absolute = false, cwd, name = 'filepath' } = options
   if (!isIdentifierName(name)) {
     throw new Error(`Name this should be a valid identifier, got: ${JSON.stringify(name)}`)
